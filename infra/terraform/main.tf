957a3c0eed21fca78784498c737407dc503afac0
Rodrigo Castro <55765970+rzavalik@users.noreply.github.com>
2025-05-29 06:13:28 -0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_log_analytics_workspace" "tictactoe_logs" {
   location            = azurerm_resource_group.tictactoe_rg.location
   resource_group_name = azurerm_resource_group.tictactoe_rg.name
   sku                 = "PerGB2018"
-  retention_in_days = 7
+  retention_in_days   = 30
 }
 
 # Prepare the Container Environment (tictactoe_env)
